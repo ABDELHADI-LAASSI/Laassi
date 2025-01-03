0c0339d7a0c3ba783ffb18fc693862a1c4dc0dbc
Abdelhadi <twitchabdelhadi@gmail.com>
2025-01-03 10:22:19 +0100

    finishing home and start testing

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './home.css'
+import Abdelhadi from '../../images/Eleric.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
   return (
     <div className='home' id="home">
         <div className="container">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati debitis vel harum illum necessitatibus quo officiis, provident laudantium dignissimos saepe nisi, magni excepturi velit ipsam praesentium fugit modi reprehenderit id aperiam numquam, quos quae? Magni distinctio, libero aliquam repudiandae quas repellat odit nisi, incidunt dignissimos omnis doloribus eveniet eum delectus voluptas est. A atque quisquam consectetur consequuntur ex debitis fugiat incidunt qui dolor accusantium odit, culpa veniam blanditiis ullam itaque laborum. Nobis autem praesentium doloremque maiores rem fuga recusandae laborum, quis, quisquam quas cumque beatae optio id dignissimos labore placeat blanditiis, nulla veniam voluptates veritatis distinctio. Distinctio dolorum consequuntur deserunt.
+          <div className="home_content">
+            <div className="text_content">
+              <h6>Available for work</h6>
+              <h1>My Name is <span>Abdelhadi</span></h1>
+              <h3>Developpeur Full Stack</h3>
+              <p>Transforming Ideas into Seamless Digital Solutions</p>
+              <div className="hire_me_button">
+                <span>Hire Me</span>
+                <a href="#contact">
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </a>
+              </div>
+            </div>
+            <div className="image_content">
+            <div class="outer-circle">
+              <div class="dot"></div>
+            </div>
+
+            <div class="inner-circle">
+              <div class="dot"></div>
+            </div>
+
+            <div class="profile-image">
+              <img src={Abdelhadi} alt="Profile" />
+            </div>
+            </div>
+          </div>
         </div>
     </div>
   )
